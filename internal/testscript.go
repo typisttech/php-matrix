c9f44b1723670dce0c1fff3116f1a8ee5c44db6a
TangRufus <tangrufus@gmail.com>
2026-04-29 13:33:17 +0100

    wip

--- internal/testscript.go
+++ internal/testscript.go
@@ -8,7 +8,7 @@ import (
 )
 
 func RunTestscript(t *testing.T) {
-    testscript.Run(t, testscript.Params{
+	testscript.Run(t, testscript.Params{
 		Dir: "testdata",
 		Setup: func(env *testscript.Env) error {
 			env.Setenv("COLUMNS", "120") // https://github.com/symfony/console/blob/2b9c5fafbac0399a20a2e82429e2bd735dcfb7db/Style/SymfonyStyle.php#L44
